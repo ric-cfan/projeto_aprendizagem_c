cd88744fe4a3756086885cfc4af144e2e37f2e64
Ricardo <ric_cfan@hotmail.com>
2024-09-01 15:46:54 -0300

    Adicionando projeto de aprendizado API .NET baseado no guia da microsoft

---
+++ ProjetoAprendizadoC/ProjetoAprendizadoC/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ProjetoAprendizadoC.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
